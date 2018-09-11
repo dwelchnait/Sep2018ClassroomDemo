c68b99bea850c92986848a038717633d277e41ac
Don Welch <dwelch@nait.ca>
2018-09-10 20:44:17 -0600

    set up Dontext class and config file

---
+++ ChinookCS/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using Chinook.Data.Entities;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    internal class ChinookContext:DbContext
+    {
+        public ChinookContext():base("ChinookDB")
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
