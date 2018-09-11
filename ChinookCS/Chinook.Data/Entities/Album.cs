ba1add242bae2d95b2ff611de2f84b821317a54c
Don Welch <dwelch@nait.ca>
2018-09-10 20:07:06 -0600

    cretae classroom chinook solution with entities

---
+++ ChinookCS/Chinook.Data/Entities/Album.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(160, ErrorMessage = "Title is limited to 160 characters")]
+        public string Title { get; set; }
+        public int ArtistId {get;set;}
+        public int ReleaseYear { get; set; }
+        [StringLength(50, ErrorMessage = "Release Label is limited to 50 characters")]
+        public string ReleaseLabel { get; set; }
+    }
+}
---
+++ ChinookCS/Chinook.Data/Entities/Artist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+namespace Chinook.Data.Entities
+{
+    [Table("Artists")]
+    public class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(120,ErrorMessage ="Name is limited to 120 characters")]
+        public string Name { get; set; }
+    }
+}
