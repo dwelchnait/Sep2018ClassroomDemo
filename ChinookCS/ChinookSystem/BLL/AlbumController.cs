f6682259cdac60d12fe447e2079766d24c78cc22
Don Welch <dwelch@nait.ca>
2018-09-12 20:12:41 -0600

    creating controllers

---
+++ ChinookCS/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;  //ODS
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Album> Album_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Album Album_Find(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+    }
+}
+
---
+++ ChinookCS/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel; //ODS
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class ArtistController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Artist> Artist_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public Artist Artist_Find(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistid);
+            }
+        }
+    }
+}
